9ba74e9a0b9bf3bc887f62b0b0e857137e595ecd
Baratashvili <student.nikoloz.baratashvili@gmail.com>
2024-04-02 04:08:53 +0400

    Added DbContext Class

---
+++ Community.DBC/CommunityDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Community.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace Community.DBC;
+
+public class CommunityDBContext : DbContext
+{
+    public CommunityDBContext(DbContextOptions<CommunityDBContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Person> People { get; set; }
+    public DbSet<ContactInfo> ContactInfos { get; set; }
+    public DbSet<Relationship> Relationships { get; set; }
+
+}
