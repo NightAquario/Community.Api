27d49123d240b4e51f26d9d2c6bc908c4e0e3637
Baratashvili <student.nikoloz.baratashvili@gmail.com>
2024-04-02 04:44:54 +0400

    Repository integration start

---
+++ Community.Repository/RepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using Community.DBC;
+using Microsoft.EntityFrameworkCore;
+
+namespace Community.Repositories;
+
+internal abstract class RepositoryBase<T>
+{
+    protected readonly CommunityDBContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    internal RepositoryBase(CommunityDBContext context, DbSet<T> dbSet)
+    {
+        _context = context;
+        _dbSet = dbSet;
+    }
+}
