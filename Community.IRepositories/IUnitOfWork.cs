e00d2f83b25b4ae8bc288190e5ae122f0c217a09
Baratashvili <student.nikoloz.baratashvili@gmail.com>
2024-04-03 13:41:33 +0400

    UnitOfWork implementation

---
+++ Community.IRepositories/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿namespace Community.IRepositories;
+
+public interface IUnitOfWork : IDisposable
+{
+    ICityRepository CityRepository { get; }
+    IContactInfoRepository ContactInfoRepository { get; }
+    IPersonRepository PersonRepository { get; }
+    IRelationshipRepository RelationshipRepository { get; }
+    int SaveChanges();
+    void BeginTransaction();
+    void CommitTransaction();
+    void RollBack();
+}
---
+++ Community.Repository/UnitOfWork.cs
@@ -0,0 +1,73 @@
+﻿using Community.DBC;
+using Community.IRepositories;
+using Community.Repositories;
+
+namespace Community.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly CommunityDBContext _context;
+    private readonly Lazy<ICityRepository> _cityRepository;
+    private readonly Lazy<IContactInfoRepository> _contactInfoRepository;
+    private readonly Lazy<IPersonRepository> _personRepository;
+    private readonly Lazy<IRelationshipRepository> _relationshipRepository;
+
+    public UnitOfWork(CommunityDBContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+        _cityRepository = new Lazy<ICityRepository>(() => new CityRepository(context));
+        _contactInfoRepository = new Lazy<IContactInfoRepository>(() => new ContactInfoRepository(context));
+        _personRepository = new Lazy<IPersonRepository>(() => new PersonRepository(context));
+        _relationshipRepository = new Lazy<IRelationshipRepository>(() => new RelationshipRepository(context));
+    }
+
+    public ICityRepository CityRepository => _cityRepository.Value;
+    public IContactInfoRepository ContactInfoRepository => _contactInfoRepository.Value;
+    public IPersonRepository PersonRepository => _personRepository.Value;
+    public IRelationshipRepository RelationshipRepository => _relationshipRepository.Value;
+
+    public int SaveChanges() =>
+        _context.SaveChanges();
+    public void BeginTransaction()
+    {
+        if (_context.Database.CurrentTransaction != null)
+        {
+            throw new InvalidOperationException("A Transaction is already in progress.");
+        }
+
+        _context.Database.BeginTransaction();
+    }
+
+    public void CommitTransaction()
+    {
+        try
+        {
+            _context.Database.CurrentTransaction?.Commit();
+        }
+        catch
+        {
+            _context.Database.CurrentTransaction?.Rollback();
+            throw;
+        }
+        finally
+        {
+            _context.Database.CurrentTransaction?.Dispose();
+        }
+    }
+
+    public void Dispose() =>
+        _context.Dispose();
+
+    public void RollBack()
+    {
+        try
+        {
+            _context.Database.CurrentTransaction?.Rollback();
+        }
+        finally
+        {
+            _context.Database.CurrentTransaction?.Dispose();
+        }
+    }
+
+}
