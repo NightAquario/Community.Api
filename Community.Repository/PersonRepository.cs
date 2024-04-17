cc519ee88e5d0ab2d7e1082fa966f38658cea883
Baratashvili <student.nikoloz.baratashvili@gmail.com>
2024-04-17 15:39:11 +0400

    Removed CommunityTestDBContext Modified PersonRepository LINQ

--- Community.Repository/PersonRepository.cs
+++ Community.Repository/PersonRepository.cs
@@ -11,15 +11,27 @@ internal sealed class PersonRepository : RepositoryBase<Person>, IPersonReposito
 
     }
 
-    public IEnumerable<Relationship> GetAllRelationships(int personId)
+    public IEnumerable<Person> GetAllPeopleWithRelationships(int personId)
     {
-        var relationships = from person in _context.People
-                            join relation in _context.Relationships
-                            on person.Id equals relation.FromPersonId into personRelations
-                            from pr in personRelations.DefaultIfEmpty()
-                            where (pr.FromPersonId == personId || pr.ToPersonId == personId) && person.Id != personId
-                            select pr;
+        var peopleWithRelationships = from person in _context.People
+                                      join relation in _context.Relationships
+                                      on person.Id equals relation.FromPersonId into personRelations
+                                      from pr in personRelations.DefaultIfEmpty()
+                                      where (pr.FromPersonId == personId || pr.ToPersonId == personId) && person.Id != personId
+                                      select person;
 
-        return relationships.ToList();
+        return peopleWithRelationships;
+    }
+
+    public IEnumerable<Person> GetAllPeopleWithRelationships(Person person)
+    {
+        var peopleWithRelationships = from p in _context.People
+                                      join r in _context.Relationships
+                                      on p.Id equals r.FromPersonId into personRelations
+                                      from pr in personRelations.DefaultIfEmpty()
+                                      where (pr.FromPersonId == person.Id || pr.ToPersonId == person.Id) && p.Id != person.Id
+                                      select p;
+
+        return peopleWithRelationships;
     }
 }
